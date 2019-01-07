94b53c20d2cceeebce2a41cfb2f3b851c773a919
Aibek <aibek.zhil@gmail.com>
2019-01-07 16:15:48 -0500

    Clean Wrapper

--- FabricStuff/Wrapper.py
+++ FabricStuff/Wrapper.py
@@ -7,8 +7,6 @@ def wrapper(fn, ip, password):
     connect = Connection(ip, connect_kwargs={"password": password}, config=config)
     try:
         fn(connect)
-        # connect.run("history -c")
-        # connect.sudo("history -c")
         print(f"{ip} executed successfully")
     except invoke.exceptions.UnexpectedExit:
         print(f"Command Failed On {ip}")
